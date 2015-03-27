3ea2323c559971625b8ea26af8501c9c917c5c26
Dave Taylor <davehtaylor@me.com>
2015-03-26 21:19:40 -0400

    Added exercise 11

---
+++ c/ex_11.c
@@ -0,0 +1,32 @@
+/* Define a function generate_n_chars() that takes an integer n and a
+ * character c and returns a string, n characters long, consisting only 
+ * of c:s. For example, generate_n_chars(5,"x") should return the 
+ * string "xxxxx".
+ */
+
+#include <stdio.h>
+
+void generate_n_chars(char, int);
+
+int main(void)
+{
+    char userchar;
+    int usernum;
+
+    printf("Enter a character and a number: ");
+    scanf(" %c %d", &userchar, &usernum);
+
+    generate_n_chars(userchar, usernum);
+    return 0;
+}
+
+void generate_n_chars(char input_char, int input_num)
+{
+    int i;
+
+    for (i = 0; i < input_num; i++)
+    {
+        putchar(input_char);
+    }
+    putchar('\n');
+}
