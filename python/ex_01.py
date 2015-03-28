0bbe5e554425cfc8e7ff06b78580b9590a14dc44
Dave Taylor <davehtaylor@me.com>
2015-03-28 16:00:43 -0400

    Changed max() function name. There is a built-in max function, so i don't want it to overlap.

--- python/ex_01.py
+++ python/ex_01.py
@@ -2,7 +2,7 @@
 # the largest of them.
 
 
-def max(inputNum1, inputNum2):
+def maxNum(inputNum1, inputNum2):
     """Find the largest of two input numbers"""
     if inputNum1 > inputNum2:
         return inputNum1
@@ -13,4 +13,4 @@ print("Please enter a number:")
 userNum1 = input()
 print("Please enter a second number:")
 userNum2 = input()
-print("The largest of the two is " + max(userNum1, userNum2))
+print("The largest of the two is " + maxNum(userNum1, userNum2))
